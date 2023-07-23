a2680102ec2623c235e02e585145bde057f00a63
unknown <Amritanshu>
2023-07-23 20:56:10 +0530

    error fixed

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,5 @@
 import Navbar from "@/components/navbar";
-import Sidebar from "@/components/sidebar";
+
 
 const DashboardLayout = ({
     children
@@ -11,7 +11,6 @@ const DashboardLayout = ({
             <div className="hidden h-full md:flex md:w-72
             md:flex-col md:fixed md:insert-y-0 z-[80]
             bg-gray-900">
-               <Sidebar />
             </div>
         
         <main className="md:pl-72">
