811bff9ada993cf98850f5dfee945935e2dc5d37
unknown <Amritanshu>
2023-07-23 21:02:39 +0530

    fixed

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,7 +8,7 @@ import MobileSidebar from "@/components/mobile-sidebar";
 const Navbar = () => {
     return(
         <div className="flex items-center p-4">
-            <MobileSidebar />
+           
             <div className="flex w-full justify-end">
                 <UserButton afterSignOutUrl="/" />
             </div>
